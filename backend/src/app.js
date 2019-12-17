9c89bbe5fd5672f331c283c6ee0f728939921f0b
Jonathan Barros <jonabf1@gmail.com>
2019-12-17 17:36:08 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,23 +27,6 @@ app.use((req, res, next) => {
   return next();
 })
 
-
-/*
-var clients = [];
-io.on('connection', function (client) {
-  client.on("join", function (name) {
-    console.log("Joined: " + name);
-    clients.push({ name, id: client.id })
-    client.emit("update", clients);
-  })
-
-  client.on('disconnect', function () {
-    io.emit("update", clients + " has left the server.")
-    clients.splice(clients.indexOf(client), 1);
-  }); 
-});
- */
-
 app.use(cors());
 app.use(express.json());
 app.use(routes);
